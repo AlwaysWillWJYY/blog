4c12623c50b386d1a414e5bb48c6b0d1652eb970
像南瓜默默成长 <929654219@qq.com>
2022-08-21 20:13:47 +0800

    update logo和锁

--- vuepress-theme-reco-demo-demo-1.x/.vuepress/config.js
+++ vuepress-theme-reco-demo-demo-1.x/.vuepress/config.js
@@ -1,102 +1,3 @@
-// module.exports = {
-//   title: "vuepress-theme-reco",
-//   description: 'A simple and beautiful vuepress blog theme .',
-//   dest: 'public',
-//   head: [
-//     ['link', { rel: 'icon', href: '/favicon.ico' }],
-//     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
-//   ],
-//   theme: 'reco',
-//   themeConfig: {
-//     nav: [
-//       { text: 'Home', link: '/', icon: 'reco-home' },
-//       { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-//       { text: 'Docs', 
-//         icon: 'reco-message',
-//         items: [
-//           { text: 'vuepress-reco', link: '/docs/theme-reco/' }
-//         ]
-//       },
-//       { text: 'Contact', 
-//         icon: 'reco-message',
-//         items: [
-//           { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
-//         ]
-//       }
-//     ],
-//     sidebar: {
-//       '/docs/theme-reco/': [
-//         '',
-//         'theme',
-//         'plugin',
-//         'api'
-//       ]
-//     },  
-//     type: 'blog',
-//     // 博客设置
-//     blogConfig: {
-//       category: {
-//         location: 2, // 在导航栏菜单中所占的位置，默认2
-//         text: 'Category' // 默认 “分类”
-//       },
-//       tag: {
-//         location: 3, // 在导航栏菜单中所占的位置，默认3
-//         text: 'Tag' // 默认 “标签”
-//       }
-//     },
-//     friendLink: [
-//       {
-//         title: '午后南杂',
-//         desc: 'Enjoy when you can, and endure when you must.',
-//         email: '1156743527@qq.com',
-//         link: 'https://www.recoluan.com'
-//       },
-//       {
-//         title: 'vuepress-theme-reco',
-//         desc: 'A simple and beautiful vuepress Blog & Doc theme.',
-//         avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-//         link: 'https://vuepress-theme-reco.recoluan.com'
-//       },
-//     ],
-//     logo: '/logo.png',
-//     // 搜索设置
-//     search: true,
-//     searchMaxSuggestions: 10,
-//     // 自动形成侧边导航
-//     // sidebar: 'auto',
-//     // 最后更新时间
-//     lastUpdated: 'Last Updated',
-//     // 作者
-//     author: 'reco_luan',
-//     // 作者头像
-//     authorAvatar: '/avatar.png',
-//     // 备案号
-//     record: 'xxxx',
-//     // 项目开始时间
-//     startYear: '2017'
-//     /**
-//      * 密钥 (if your blog is private)
-//      */
-
-//     // keyPage: {
-//     //   keys: ['your password'],
-//     //   color: '#42b983',
-//     //   lineColor: '#42b983'
-//     // },
-
-//     /**
-//      * valine 设置 (if you need valine comment )
-//      */
-
-//     // valineConfig: {
-//     //   appId: '...',// your appId
-//     //   appKey: '...', // your appKey
-//     // }
-//   },
-//   markdown: {
-//     lineNumbers: true
-//   }
-// }  
 module.exports = {
   base: '/',
   locales: {
@@ -148,8 +49,8 @@ module.exports = {
       appId: '1HbrYKc61YA2cpKyOIfSjxfr-gzGzoHsz',
       appKey: 'MB0cbJwSejdYlix1AjjLLFRv',
     },
-    logo: '/avatar.png',
-    authorAvatar: '/avatar.png',
+    logo: '/logo.png',
+    authorAvatar: '/logo.png',
     search: true,
     searchMaxSuggestions: 5,
     subSidebar: 'auto',
@@ -165,6 +66,12 @@ module.exports = {
     //     '/',
     //   ],
     // }],
+    // 密钥
+    // keyPage: {
+    //   keys: ['271fb7d9e6f1e9507f6fd0f77439fd4a'], // 1.3.0 版本后需要设置为密文
+    //   color: '#42b983', // 登录页动画球的颜色
+    //   lineColor: '#42b983' // 登录页动画线的颜色
+    // },
     sidebarDepth: 4,
     lastUpdated: 'Last Updated',
     author: 'AuraroJ',
@@ -172,9 +79,6 @@ module.exports = {
     // record: 'xxxxxx',
     // recordLink: '',
     startYear: '2022',
-    /**
-     * 密钥 (if your blog is private)
-     */
     friendLink: [
       {
         title: '午后南杂',
